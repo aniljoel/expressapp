3e1aaad1092061e0697ab543718cab0a49b27a44
Anil Kondichetty <aniljoel@gmail.com>
2015-03-17 16:58:32 -0700

    modified index.js

--- myfirstexpressapp/routes/index.js
+++ myfirstexpressapp/routes/index.js
@@ -5,5 +5,14 @@ var router = express.Router();
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
+router.post('/',function(req,res,next){
+    res.send('this is a post');
+});
+router.delete('/', function (req, res,next) {
+    res.send('Got a DELETE request at /user');
+})
+router.put('/', function (req, res,next) {
+    res.send('Got a PUT request at /user');
+});
 
 module.exports = router;
